cd33ffecaf81baaa09a22a4ab362f571638a7e43
jose Tovar <jose.tovar102397@gmail.com>
2022-01-19 10:46:03 -0700

    Create post request

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -2,3 +2,14 @@ export const getOrders = () => {
   return fetch('http://localhost:3001/api/v1/orders')
       .then(response => response.json())
 }
+
+export const submitOrder = (order) => {
+  return fetch('http://localhost:3001/api/v1/orders', {
+    method: "POST",
+    body: JSON.stringify(order),
+    headers: {
+      "Content-type": "application/json"
+    }
+  })
+  .then(res => res.json())
+}
